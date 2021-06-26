246752bbb5c5c214830ff1b204681b743ee10298
shilpi priya <36822239+shilpihjp12@users.noreply.github.com>
2021-06-26 13:01:08 +0200

    Initial commit

---
+++ src/buyflow/SummaryStep.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+interface SummaryStepProps {
+  collectedData: {
+    name: string;
+    email: string;
+    age: number;
+  };
+}
+
+const SummaryStep: React.FC<SummaryStepProps> = (props) => {
+  return (
+    <>
+      <div>Name: {props.collectedData.name}</div>
+      <div>Email: {props.collectedData.email}</div>
+      <div>Age: {props.collectedData.age}</div>
+      <div>
+        <Link to="/purchased=dev_ins">Purchase</Link>
+      </div>
+    </>
+  );
+};
+
+export default SummaryStep;
---
+++ src/configuration/workflowConf.tsx
@@ -0,0 +1,16 @@
+/**
+ * This file is created to keep all workflow related configurations.
+ * If new workflow is getting added in future, this file can get utilize for the same.
+ *
+ */
+export enum ProductIds {
+  devIns = "dev_ins"
+}
+
+export interface BuyflowProps {
+  productId: ProductIds;
+}
+
+export const PRODUCT_IDS_TO_NAMES = {
+  [ProductIds.devIns]: "Developer Insurance"
+};
